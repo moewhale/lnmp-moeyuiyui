4321c5cdfbaef10dbd115c5e8cfb6f57c812467f
MoeWhale <51768867+moewhale@users.noreply.github.com>
2025-09-29 15:02:12 +0800

    Update version.sh

--- include/version.sh
+++ include/version.sh
@@ -80,9 +80,9 @@ elif [ "${PHPSelect}" = "12" ]; then
 elif [ "${PHPSelect}" = "13" ]; then
     Php_Ver='php-8.2.29'
 elif [ "${PHPSelect}" = "14" ]; then
-    Php_Ver='php-8.3.25'
+    Php_Ver='php-8.3.26'
 elif [ "${PHPSelect}" = "15" ]; then
-    Php_Ver='php-8.4.12'
+    Php_Ver='php-8.4.13'
 fi
 if [[ "${PHPSelect}" =~ ^[123]$ ]]; then
     PhpMyAdmin_Ver='phpMyAdmin-4.0.10.20-all-languages'
